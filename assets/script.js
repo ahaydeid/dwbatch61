18f4b159686e813834fb83f17eb7d4780e1c5f82
ahaydeid <adi.hadi270@gmail.com>
2025-05-08 01:26:27 +0700

    Update lagi

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,18 @@
+// Ambil nilai dan tampilkan pilihan subject
+let selectedSubject = ""; // variabel untuk menyimpan subject yang dipilih, dibuat global supaya bisa diakses function getData()
+let items = document.querySelectorAll(".dropdown-item");  // Ambil semua element HTML yang punya class .dropdown-item
+let dropdownText = document.getElementById("dropdownText"); // Lokasi ngasih value elemen yang dipilih
+
+items.forEach(item => {  // Looping semua item yang ada di dropdown
+  item.addEventListener("click", function () { // Ketika item di klik
+    // Ambil value dari item yang di klik
+    dropdownText.textContent = this.textContent; // Tampilkan value yang dipilih di dropdown
+    selectedSubject = this.textContent; // Simpan nilai yang dipilih
+  });
+});
+
+
+
 // Fungsi untuk ambil value dari inputan biasa
 function getData(event){
     event.preventDefault()
